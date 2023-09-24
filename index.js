ce99fb0be98c6025bdf1cbbb34c56d404c2b90f9
Junaid <aliakhtar7890@gmail.com>
2023-09-24 13:19:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ $(".programmer").hide().fadeIn(1500);
 
 $(".navbar").hide().fadeIn(1500);
 
-$
+$(".mountain").hide().fadeIn(1500);
+
+$(".top-cloud").hide().fadeIn(1500);
+
+$(".bottom-cloud").hide().fadeIn(1500);
